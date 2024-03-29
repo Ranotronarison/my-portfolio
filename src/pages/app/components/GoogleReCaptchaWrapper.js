27276345c5ccab67bc13534f228e4a824b37c9b4
ranotronarison <arivonyran@gmail.com>
2024-03-29 12:19:45 +0300

    fix white space

--- src/pages/app/components/GoogleReCaptchaWrapper.js
+++ src/pages/app/components/GoogleReCaptchaWrapper.js
@@ -3,18 +3,18 @@
 import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 
 export default function GoogleReCaptchaWrapper({ children }) {
-    const recaptchaKey = process.env.NEXT_PUBLIC_G_RECAPTCHA_SITE_KEY;
-    return (
-        <GoogleReCaptchaProvider
-            reCaptchaKey={recaptchaKey ?? "NOT DEFINED"}
-            scriptProps={{
-                async: false,
-                defer: false,
-                appendTo: "head",
-                nonce: undefined,
-            }}
-        >
-            {children}
-        </GoogleReCaptchaProvider>
-    )
+  const recaptchaKey = process.env.NEXT_PUBLIC_G_RECAPTCHA_SITE_KEY;
+  return (
+    <GoogleReCaptchaProvider
+      reCaptchaKey={recaptchaKey ?? "NOT DEFINED"}
+      scriptProps={{
+        async: false,
+        defer: false,
+        appendTo: "head",
+        nonce: undefined,
+      }}
+    >
+      {children}
+    </GoogleReCaptchaProvider>
+  )
 }
