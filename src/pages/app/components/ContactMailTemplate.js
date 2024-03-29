9001a4a5bd58ae6cfd07c47ef60df253fd227cfa
ranotronarison <arivonyran@gmail.com>
2024-03-29 12:20:59 +0300

    add export default to ContactMailTemplate

--- src/pages/app/components/ContactMailTemplate.js
+++ src/pages/app/components/ContactMailTemplate.js
@@ -1,11 +1,13 @@
 import React from 'react'
-export const ContactMailTemplate = ({ email, name, message }) => (
-  <pre>
-    <dl>
-      <dt>Message from :</dt>
-      <dd>{name} | {email}</dd>
-      <dt>Message :</dt>
-      <dd>{message}</dd>
-    </dl>
-  </pre>
-)
+export default function ContactMailTemplate({ email, name, message }) {
+  return (
+    <pre>
+      <dl>
+        <dt>Message from :</dt>
+        <dd>{name} | {email}</dd>
+        <dt>Message :</dt>
+        <dd>{message}</dd>
+      </dl>
+    </pre>
+  )
+}
