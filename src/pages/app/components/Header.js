3a18d6e4bad16afe2cdcee227ba402e4accea546
ranotronarison <arivonyran@gmail.com>
2024-08-28 19:06:55 +0300

    feat: update navbar layout

--- src/pages/app/components/Header.js
+++ src/pages/app/components/Header.js
@@ -1,5 +1,7 @@
 'use client'
 
+import { cn } from "@/lib/utils";
+import { MenuIcon } from "lucide-react";
 import Link from "next/link";
 import { useState } from "react";
 
@@ -11,17 +13,13 @@ export default function Header() {
   };
 
   return <nav className="fixed top-0 left-0 w-full z-50 bg-white">
-    <div className="flex justify-center items-center px-4 py-2">
-      <button onClick={toggleMenu} className="md:hidden focus:outline-none">
-        <svg
-          className="h-6 w-6 "
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path d="M4 6H20M4 12H20M4 18H20" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-        </svg>
-      </button>
+    <div className="container flex justify-between items-center px-4 py-2">
+      <div className="flex items-center gap-2">
+        <button onClick={toggleMenu} className="md:hidden focus:outline-none">
+          <MenuIcon />
+        </button>
+        <Link href={"/"} className="text-2xl">Nomena R.</Link>
+      </div>
 
       <ul className="hidden md:flex items-center space-x-4">
         <li>
@@ -56,7 +54,7 @@ export default function Header() {
     </div>
 
     {isOpen && (
-      <ul className="md:hidden absolute top-full left-0 w-full py-4 bg-white border-b">
+      <ul className={cn("md:hidden absolute top-full left-0 w-full py-4 bg-white border-b")}>
         <li>
           <Link href="#home" className="flex items-center space-x-2  px-3 py-2 hover:text-gray-500">
             <svg className="w-5 h-5 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
