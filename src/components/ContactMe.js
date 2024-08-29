9292e7bdfe374bfd060a1f37ba8e292275f46a36
ranotronarison <arivonyran@gmail.com>
2024-08-29 12:21:10 +0300

    fix: remove methode attribute ono contact form

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -16,7 +16,7 @@ export default function ContactMe() {
     <section id="contact-me">
       <div className="md:container mx-auto px-2 md:px-46 w-full mb-48">
         <h3 className="section-title">{'{ Contact me }'}</h3>
-        <form action={formAction} className="space-y-8 mx-auto w-full md:w-1/2" method="POST">
+        <form action={formAction} className="space-y-8 mx-auto w-full md:w-1/2">
           <div className="w-full">
             <input
               className="w-full rounded-sm p-2"
