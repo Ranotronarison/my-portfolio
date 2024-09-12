e6be6fb993115af7bc21e0e496cea8897d51a0ee
ranotronarison <arivonyran@gmail.com>
2024-09-12 14:02:21 +0300

    feat: translate metadata

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -5,18 +5,27 @@ import { SpeedInsights } from "@vercel/speed-insights/next"
 import localFont from 'next/font/local'
 import { NextIntlClientProvider } from "next-intl";
 import { localeConfig } from "@/config/locale.config";
-import { getMessages } from "next-intl/server";
+import { getMessages, getTranslations } from "next-intl/server";
 
 const myFont = localFont({ src: '../../../public/fonts/RobotoSlab-VariableFont_wght.ttf' })
 
-export const metadata = {
-  title: "Nomena R. | Web Developer",
-  description: "Experimented Web developer from Madagascar. Symfony, React, PHP, JavaScript, TypeScript and more ...",
-  keywords: ['Web Developer', 'Fullstack', 'Symfony', 'NodeJs', 'React', 'Madagascar', 'Nomena'],
-  alternates: {
-    canonical: process.env.WEBSITE_URL,
-  },
-};
+export async function generateMetadata({ params: { locale } }) {
+  const t = await getTranslations({ locale, namespace: 'Metadata' });
+  return {
+    title: t('title'),
+    description: t('description'),
+    keywords: ["Web Developer",
+      "Fullstack",
+      "Symfony",
+      "NodeJs",
+      "React",
+      "Madagascar",
+      "Nomena"],
+    alternates: {
+      canonical: process.env.WEBSITE_URL,
+    },
+  }
+}
 
 const { locales } = localeConfig
 
